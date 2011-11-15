1a2fc9896d0c9693caeedf7bef688bb29591086d
Alex Soulim <soulim@gmail.com>
2011-11-15 20:39:50 +0400

    Add specs for VacancyMailer

--- spec/mailers/vacancy_mailer_spec.rb
+++ spec/mailers/vacancy_mailer_spec.rb
@@ -1,5 +1,35 @@
 require "spec_helper"
 
 describe VacancyMailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:vacancy) do
+    stub_model(Vacancy, {
+      :title       => "Foo",
+      :description => "Lorem ipsum",
+      :company     => "Foo Inc.",
+      :email       => "person@example.com",
+      :expire_at   => 1.week.from_now.to_date
+    })
+  end
+  
+  describe "creation_notice" do
+    before{ @email = VacancyMailer.creation_notice(vacancy).deliver }
+    
+    it "should put email into queue" do
+      ActionMailer::Base.deliveries.should_not be_blank
+    end
+    it "should deliver message to 'support@rubyjobs.ru'" do
+      @email.to.should include('support@rubyjobs.ru')
+    end
+  end
+
+  describe "approval_notice" do
+    before{ @email = VacancyMailer.approval_notice(vacancy).deliver }
+
+    it "should put email into queue" do
+      ActionMailer::Base.deliveries.should_not be_blank
+    end
+    it "should deliver message to vacancy's owner" do
+      @email.to.should include(vacancy.email)
+    end
+  end
 end
