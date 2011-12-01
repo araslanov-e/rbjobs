9dfb119a5f45076030f81fc1c6682de1f0d48f2f
Alex Soulim <soulim@gmail.com>
2011-12-01 21:26:25 +0400

    Update email initializer

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -1,5 +1,5 @@
 config = YAML.load_file(File.join(Rails.root, "config", "email.yml"))[Rails.env]
 
-Rbjobs::Application.config.action_mailer.delivery_method     = config["delivery_method"]
-Rbjobs::Application.config.action_mailer.default_url_options = config["default_url_options"]
-Rbjobs::Application.config.action_mailer.postmark_settings   = config["postmark_settings"] unless config["postmark_settings"].blank?
+ActionMailer::Base.delivery_method     = config["delivery_method"].to_sym
+ActionMailer::Base.default_url_options = config["default_url_options"]
+ActionMailer::Base.postmark_settings   = config["postmark_settings"].symbolize_keys unless config["postmark_settings"].blank?
