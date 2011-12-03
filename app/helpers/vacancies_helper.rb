05a3b67a4f6e8a6e9936a1ca386a1ee3a93f7131
Alex Soulim <soulim@gmail.com>
2011-12-03 12:19:46 +0400

    Add company and location to title of vacancy

--- app/helpers/vacancies_helper.rb
+++ app/helpers/vacancies_helper.rb
@@ -1,2 +1,5 @@
 module VacanciesHelper
+  def company_location_tag(vacancy)
+    [vacancy.company, vacancy.location].reject{ |i| i.blank? }.join(" - ")
+  end
 end
--- spec/helpers/vacancies_helper_spec.rb
+++ spec/helpers/vacancies_helper_spec.rb
@@ -1,15 +1,37 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the VacanciesHelper. For example:
-#
-# describe VacanciesHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       helper.concat_strings("this","that").should == "this that"
-#     end
-#   end
-# end
 describe VacanciesHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:vacancy){ stub_model(Vacancy, :company => "Foo Inc.", :location => "Neverland") }
+  
+  describe "company_location_tag" do
+    context "when company and location are presented" do
+      it "concats company and location with dash" do
+        helper.company_location_tag(vacancy).should == "#{vacancy.company} - #{vacancy.location}"
+      end
+    end
+    context "when company is blank" do
+      before{ vacancy.company = nil }
+      
+      it "returns only location string" do
+        helper.company_location_tag(vacancy).should == vacancy.location
+      end
+    end
+    context "when location is blank" do
+      before{ vacancy.location = nil }
+      
+      it "returns only company string" do
+        helper.company_location_tag(vacancy).should == vacancy.company
+      end
+    end
+    context "when company and location are blank" do
+      before do
+        vacancy.company = nil
+        vacancy.location = nil
+      end
+      
+      it "returns empty string" do
+        helper.company_location_tag(vacancy).should be_blank
+      end
+    end
+  end
 end
